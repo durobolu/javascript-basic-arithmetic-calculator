d232f5e632a45cf93fab5ba87b2a03047016fdec
durobolu <durobolu@gmail.com>
2022-06-25 00:07:45 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,20 @@
+let x, y;
+x = parseFloat(prompt("Enter first number: "));
+y = parseFloat(prompt("Enter second number: "));
+addition = x + y;
+substraction = x - y;
+multiplication = x * y;
+division = x / y;
+
+//addition operation
+alert(addition);
+console.log("The sum of ", x, "and", y, "is", addition);
+//subtraction operation
+alert(substraction);
+console.log("The substraction of ", y, "from", x, "is", substraction);
+//multiplication operation
+alert(multiplication);
+console.log("The multiple of ", x, "and", y, "is", multiplication);
+//division operation
+alert(division);
+console.log("The division of ", x, "and", y, "is", division);
